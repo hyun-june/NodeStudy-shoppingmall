67e8f031737544b661021e5a281e643d0c319d3c
Hyunjun Lee <leehyunjune11@gmail.com>
2024-10-25 20:37:58 +0900

    [241025] deploy test2

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api", indexRouter);
-const mongoURI = process.env.LOCAL_DB_ADDRESS;
+const mongoURI = process.env.MONGODB_URI_PROD;
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
   .then(() => console.log("mongoose connected"))
