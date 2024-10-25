a1977f276a68a215d4330d656bb9c4078ec4dc11
Hyunjun Lee <leehyunjune11@gmail.com>
2024-10-25 21:09:09 +0900

    [241025] deploy test3

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,6 @@ mongoose
   .then(() => console.log("mongoose connected"))
   .catch((err) => console.log("DB connection fail", err));
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 10000, () => {
   console.log("server on");
 });
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -6,7 +6,7 @@ const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 // const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
 // console.log("proxy", BACKEND_PROXY);
 const api = axios.create({
-  baseURL: BACKEND_URL,
+  baseURL: `${BACKEND_URL}/api`,
   headers: {
     "Content-Type": "application/json",
     authorization: `Bearer ${sessionStorage.getItem("token")}`,
