96a6ecaec0c7716827069a52da5f9a7b1a3b7c96
Hyunjun Lee <leehyunjune11@gmail.com>
2024-10-25 18:47:40 +0900

    "[241025] 5~6"

--- backend/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userApi = require("./user.api");
-
-router.use("/user", userApi);
-
-module.exports = router;
--- backend/routes/user.api.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("./../controllers/user.controller");
-
-//회원가입
-router.post("/", userController.createUser);
-
-module.exports = router;
