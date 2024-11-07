942376f6fe6a9d6c57786a2e613f97a664552266
Hyunjun Lee <leehyunjune11@gmail.com>
2024-11-07 21:10:18 +0900

    [241107] google login

--- frontend/src/page/LoginPage/LoginPage.js
+++ frontend/src/page/LoginPage/LoginPage.js
@@ -30,6 +30,7 @@ const Login = () => {
 
   const handleGoogleLogin = async (googleData) => {
     //구글 로그인 하기
+    console.log("google Data:", googleData);
   };
 
   if (user) {
@@ -83,6 +84,13 @@ const Login = () => {
                   }}
                 />
               </GoogleOAuthProvider>
+              {/* 1. 구글 로그인 버튼 가져오기
+                  2. Oauth로그인을 위해서 google api 사이트에 가입하고 클라이언트 키, 시크릿 키 받아오기
+                  3. 로그인
+                  4. 백엔드에서 로그인하기
+                    a. 이미 로그인을 한적이 있는 유저 => 로그인시키고 토큰 값 주기
+                    b. 처음 로그인 시도를 한 유저 => 유저 정보 새로 생성 => 토큰 값 주기
+              */}
             </div>
           </div>
         </Form>
